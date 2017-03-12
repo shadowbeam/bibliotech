dbff943464f2aaa924711a5af98c6964be92e99c
Allan Watson <allanwatson@Allans-iMac.local>
2017-03-12 15:06:26 +0000

    display books

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,19 +1,13 @@
 import React, { Component } from 'react';
 import './App.less';
+import MostPopularShelf from './most-popular-shelf/MostPopularShelf';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+export default class App extends Component {
+    render() {
+        return (
+            <div className="App" >
+            <MostPopularShelf></MostPopularShelf>
+            < /div>
     );
-  }
+    }
 }
-
-export default App;
